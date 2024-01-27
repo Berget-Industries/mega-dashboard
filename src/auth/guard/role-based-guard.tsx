82a5d70ffcc15eb6ca3f3825efba1406563a1063
willebergh <willebergh@outlook.com>
2024-01-27 12:28:09 +0100

    small fix

--- src/auth/guard/role-based-guard.tsx
+++ src/auth/guard/role-based-guard.tsx
@@ -16,31 +16,30 @@ type RoleBasedGuardProp = {
 export default function RoleBasedGuard({ children, sx }: RoleBasedGuardProp) {
   const { user } = useAuthContext();
 
-  if (!user?.systemAdmin) {
-    return (
-      <Container component={MotionContainer} sx={{ textAlign: 'center', ...sx }}>
-        <m.div variants={varBounce().in}>
-          <Typography variant="h3" sx={{ mb: 2 }}>
-            Permission Denied
-          </Typography>
-        </m.div>
+  return user?.systemAdmin ? (
+    <>{children}</>
+  ) : (
+    <Container component={MotionContainer} sx={{ textAlign: 'center', ...sx }}>
+      <m.div variants={varBounce().in}>
+        <Typography variant="h3" sx={{ mb: 2 }}>
+          Permission Denied
+        </Typography>
+      </m.div>
 
-        <m.div variants={varBounce().in}>
-          <Typography sx={{ color: 'text.secondary' }}>
-            You do not have permission to access this page
-          </Typography>
-        </m.div>
+      <m.div variants={varBounce().in}>
+        <Typography sx={{ color: 'text.secondary' }}>
+          You do not have permission to access this page
+        </Typography>
+      </m.div>
 
-        <m.div variants={varBounce().in}>
-          <ForbiddenIllustration
-            sx={{
-              height: 260,
-              my: { xs: 5, sm: 10 },
-            }}
-          />
-        </m.div>
-      </Container>
-    );
-  }
-  return <> {children} </>;
+      <m.div variants={varBounce().in}>
+        <ForbiddenIllustration
+          sx={{
+            height: 260,
+            my: { xs: 5, sm: 10 },
+          }}
+        />
+      </m.div>
+    </Container>
+  );
 }
