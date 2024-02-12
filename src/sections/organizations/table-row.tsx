8059b58f4684958a44edc957cd2b62ffc879e1df
Denniz Yilmaz Mega <dennizyilmaz1903@gmail.com>
2024-02-12 14:34:35 +0100

    commitar progress på listan. Ska förbereda en funktion nu för att göra lägga till användare och ta bort

--- src/sections/organizations/table-row.tsx
+++ src/sections/organizations/table-row.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { format } from 'date-fns';
+import { useNavigate } from 'react-router-dom';
 
-import { useSelectedOrgContext } from 'src/layouts/common/context/org-menu-context';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import MenuItem from '@mui/material/MenuItem';
@@ -9,13 +9,24 @@ import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import IconButton from '@mui/material/IconButton';
 import ListItemText from '@mui/material/ListItemText';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {
+  FormControl,
+  InputLabel,
+  ListSubheader,
+  OutlinedInput,
+  Select,
+  TextField,
+} from '@mui/material';
 
-import { useNavigate } from 'react-router-dom';
-import { IPlugin } from 'src/types/organization';
+import { useSelectedOrgContext } from 'src/layouts/common/context/org-menu-context';
 
 import Label from 'src/components/label';
 import Iconify from 'src/components/iconify';
 import CustomPopover, { usePopover } from 'src/components/custom-popover';
+
+import { IUser } from 'src/types/user';
+import { IPlugin } from 'src/types/organization';
 
 // eslint-disable-next-line import/no-cycle
 import ConfirmDialog from './confirmDialog';
@@ -36,14 +47,34 @@ export interface IOrganizationTableRow {
 type Props = {
   row: IOrganizationTableRow;
   selected: boolean;
+  users: IUser[];
 };
 
-export default function OrderTableRow({ row, selected }: Props) {
+export default function OrderTableRow({ row, selected, users }: Props) {
   const navigate = useNavigate();
   const [selectedOrg, selectOrg] = useSelectedOrgContext();
   const { organization } = row;
   const [open, setOpen] = React.useState(false);
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [selectedUsers, setSelectedUsers] = React.useState<string[]>([]);
   const hasActivatedPlugins = organization.plugins.some((plugin) => plugin.isActivated);
+
+  useEffect(() => {
+    const userNames: string[] = organization.users
+      .map((userId: string) => users.find((user: IUser) => user._id === userId))
+      .filter((user: IUser | undefined): user is IUser => user !== undefined)
+      .map((user: IUser) => user.name);
+
+    setSelectedUsers(userNames);
+  }, [organization, users]);
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const handleSelectUser = (event: any) => {
+    const { value } = event.target;
+    setSelectedUsers(typeof value === 'string' ? value.split(',') : value);
+  };
 
   const handleToggleDialog = () => {
     setOpen(!open);
@@ -69,22 +100,61 @@ export default function OrderTableRow({ row, selected }: Props) {
           }}
         />
       </TableCell>
-
-      <TableCell sx={{ verticalAlign: 'middle' }}>
-        <ListItemText
-          primary={organization.users.length}
-          primaryTypographyProps={{
-            typography: 'body2',
-            noWrap: true,
-            textAlign: 'center',
-          }}
-          secondaryTypographyProps={{
-            mt: 0.5,
-            component: 'span',
-            typography: 'caption',
-          }}
-        />
+      <TableCell>
+        <FormControl>
+          <Select
+            sx={{
+              height: 32,
+              '.MuiSelect-select': {
+                py: '6px',
+                fontSize: '0.875rem',
+              },
+              '.MuiSvgIcon-root': {
+                fontSize: '1rem',
+              },
+            }}
+            labelId="user-select-label"
+            multiple
+            displayEmpty
+            value={selectedUsers}
+            onChange={handleSelectUser}
+            renderValue={(selectedd) =>
+              selectedd.length > 0 ? selectedd.join(', ') : 'Inga användare'
+            }
+            IconComponent={ArrowDropDownIcon}
+            MenuProps={{
+              PaperProps: {
+                style: {
+                  maxHeight: 48 * 4.5 + 8,
+                  width: 250,
+                },
+              },
+            }}
+          >
+            <ListSubheader>
+              <TextField
+                placeholder="Sök användare..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                variant="outlined"
+                fullWidth
+                sx={{
+                  '.MuiInputBase-root': {
+                    height: '40px',
+                    width: '100%',
+                  },
+                }}
+              />
+            </ListSubheader>
+            {filteredUsers.map((user) => (
+              <MenuItem key={user._id} value={user.name}>
+                {user.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </TableCell>
+
       <TableCell sx={{ verticalAlign: 'middle' }}>
         <ListItemText
           primary={`${organization.plugins.reduce(
